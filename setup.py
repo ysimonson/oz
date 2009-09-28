12cf3ddc2c36758d77b3ac7cdea64a4412148c87
Yusuf Simonson <simonson@gmail.com>
2009-09-28 18:49:47 -0400

    setup script now export docs and error template

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
 
-setup(name='Oz',
-      version='0.2',
-      description='A set of tools for the Tornado web framework',
-      author='Yusuf Simonson',
-      url='http://github.com/ysimonson/oz',
-      packages=['oz',],
+setup(name = 'Oz',
+      version = '0.2',
+      description = 'A set of tools for the Tornado web framework',
+      author = 'Yusuf Simonson',
+      url = 'http://github.com/ysimonson/oz',
+      packages = ['oz',],
+      data_files = [
+          ('', ['README', 'LICENSE']),
+          ('oz', ['oz/error_template.html']),
+      ]
      )
